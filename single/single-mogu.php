14a9aa8caf50d35f774edc54f2003a1d180d0206
suusan11 <gawow11@gmail.com>
2019-08-02 19:29:04 -0700

    create new single page

---
+++ single/single-mogu.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miiiiiiiiiiie
+ * Date: 2019-08-02
+ * Time: 19:25
+ */
+
+get_header();
+?>
+
+    <div class="animsition">
+        <div class="sticky__container">
+            <section id="js-breakPoint" class="work__intro--black sticky container">
+                <h2>mogu</h2>
+                <p>‘mogu’ is onomatopoeia in Japanese which is like yum yum yum…<br>
+                    Foodies blog site across Vancouver and all of the world. I have strong passion about food, this is parts of my life to eat delicious food.
+                    This blog was used simple structure of WordPress though, I learned many things how to get the data or how to show the data.
+                </p>
+                <div class="detail-set">
+                    <p>#Web Design #Web Development</p>
+                    <p>WordPress, HTML, SCSS, JavaScript, XD, Photoshop</p>
+                    <p>Feb. 2019</p>
+                </div>
+                <a class="hover__link" href="https://mogu0141.com/">Visit Website</a>
+            </section>
+
+            <div class="scroll__container">
+                <section class="work__image mogu scroll__item container">
+                    <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/whole.png" alt="whole page image">
+                </section>
+            </div>
+            <!--right section available to scroll-->
+        </div>
+        <!--introduction of work-->
+
+        <div class="sticky__container">
+            <section class="work__color container sticky container">
+                <h1 class="section__title work__h1">Colors</h1>
+                <div class="work__color--item">
+                    <h4>Theme color</h4>
+                    <p>The Color of RED which is contain both of flags Canada and Japan. Expressed much of passion about food.
+                        <br><br>This is a website which is a blog, and it should be easy to read an article.
+                        In order to that, I assigned a simple design that background color is white and the font is black.
+                        The theme color of red is also an accent color to this website.
+                    </p>
+                    <div class="work__color--item--flex-top mogu-color">
+                        <div class="color--box">#B4061F</div>
+                    </div>
+                </div>
+            </section>
+
+            <div class="scroll__container">
+                <section class="work__image--color mogu scroll__item container">
+                    <div class="image__wrapper mogu">
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/color/color__header.png" alt="color image header">
+                        <p>- Header</p>
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/color/color__header2.png" alt="color image header">
+                        <p>- Header for mobile device</p>
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/color/color__icons.png" alt="color image icons">
+                        <p>- Icons</p>
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/color/work-img1.jpg" alt="mogu work image 1">
+                    </div>
+                </section>
+            </div>
+            <!--right section available to scroll-->
+        </div>
+        <!--introduction of color-->
+
+        <div class="sticky__container">
+            <section class="work__font sticky container">
+                <h1 class="section__title work__h1">Fonts</h1>
+                <div class="work__font--item mogu__small">
+                    <h4>Base - Source Han Sans JP</h4>
+                    <p>Chose it from the readability of both alphabet and Japanese.
+                        Even if I combine those fonts(alphabets and Japanese), it won't collapse each other. The text keeps the same height.
+                    </p>
+                    <div class="flex">
+                        <div class="work__font--item-each">
+                            <img class="font-mogu" src="<?php echo get_template_directory_uri()?>/dist/images/mogu/font/fontSourceJPLight.png" alt="font Source Han Sans JP Light">
+                            <p>Light</p>
+                        </div>
+                        <div class="work__font--item-each">
+                            <img class="font-mogu" src="<?php echo get_template_directory_uri()?>/dist/images/mogu/font/fontSourceJPRegular.png" alt="font Source Han Sans JP Regular">
+                            <p>Regular</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="work__font--item">
+                    <h4>Logo - Caramel Macchiato Bouncy</h4>
+                    <p>Wanted to choose some font which is related to FOOD.
+                        This font "Caramel Macchiato Bouncy" as a logo to represent the bouncy of ones heart when eating.
+                    </p>
+                    <div class="work__font--item-each">
+                        <img class="font-mogu" src="<?php echo get_template_directory_uri()?>/dist/images/mogu/font/fontCaramel.png" alt="font Caramel Macchiato Bouncy">
+                    </div>
+                </div>
+            </section>
+
+            <div class="scroll__container">
+                <section class="work__image--font mogu scroll__item container">
+                    <div class="image__wrapper mogu">
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/font/font__title.png" alt="font title image">
+                        <p>- Title</p>
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/font/font__contents.png" alt="font contents image">
+                        <p>- Contents</p>
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/font/font__heading.png" alt="font heading image">
+                        <p>- Heading</p>
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/font/font__author.png" alt="font author image">
+                        <p>- Author description</p>
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/font/font__logo.png" alt="font logo image">
+                        <p>- Logo</p>
+                    </div>
+                </section>
+            </div>
+            <!--right section available to scroll-->
+        </div>
+        <!--introduction of fonts-->
+
+        <div class="sticky__container">
+            <section class="work__feature mogu sticky container">
+                <h1 class="section__title work__h1">Layout</h1>
+                <p>Changed the thumbnail layout of the top page and category page to change the appearance.
+                    The top page has two columns so that all articles can be viewed in a list,
+                    and the category page has one column so that new articles from above can be easily understood.
+                </p>
+            </section>
+
+            <div class="scroll__container">
+                <section class="work__image--feature mogu scroll__item container">
+                    <div class="image__wrapper">
+                        <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/feature.png" alt="feature image">
+                    </div>
+                </section>
+            </div>
+            <!--right section available to scroll-->
+        </div>
+        <!--introduction of feature-->
+
+        <section class="work__link">
+            <div class="work__link--images">
+                <img src="<?php echo get_template_directory_uri()?>/dist/images/mogu/devices.jpg" alt="devices image">
+            </div>
+            <a class="common__button button__work--bottom" href="https://mogu0141.com/" target="_blank">Visit Website</a></button>
+            <div class="pagenation container">
+                <h4 class="prev"><?php previous_post_link('%link', '< previous'); ?></h4>
+                <h4 class="next"><?php next_post_link('%link', 'next >'); ?></h4>
+            </div>
+        </section>
+        <!--work image and link to Website-->
+    </div>
+    <!--animsition-->
+
+
+<?php get_footer(); ?>
